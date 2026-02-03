0bdcd7be5a6cc03d5d981f6e49f7b95d47328d7d
alexcojocaru02 <alexcojocaru02@gmail.com>
2026-02-03 14:42:28 +0200

    Some remaining changes

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   imports: [
     MatCardModule,
-    RouterModule
+    RouterModule,
+    MatIconModule,
+    MatProgressSpinnerModule
   ],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss'
